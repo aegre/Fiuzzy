3434b4e53ea4639bfcca50772b9717a5ab34f3f3
Michell Alegría <alegria.trm@gmail.com>
2019-11-27 01:15:57 -0600

    add husky and style lint config

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
 // Disable the rule due we have devDependencies here
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-styled-components';
