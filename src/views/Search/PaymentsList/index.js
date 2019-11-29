23bf39307f53f7742e9a0a1e5398fe35da01b3fb
Michell Alegría <alegria.trm@gmail.com>
2019-11-28 23:19:53 -0600

    Implement the fuzzy search in the payments list

--- src/views/Search/PaymentsList/index.js
+++ src/views/Search/PaymentsList/index.js
@@ -1,21 +1,37 @@
 // Libraries
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+import { parse } from 'query-string';
 
 // Components
 import LOADING_STATES from 'consts/loadingStates';
+import fuzzySearch from 'utils/fuzzySearch';
 import { paymentPropTypes } from './consts';
 
 const PaymentsList = ({ payments, loading }) => {
+  const [paymentsList, setPaymentsList] = useState([]);
+
+  const { search } = useLocation();
+  const { search: searchText } = parse(search);
+  useEffect(() => {
+    if (searchText) {
+      setPaymentsList(payments.filter(fuzzySearch, searchText));
+    } else {
+      setPaymentsList(payments);
+    }
+  }, [payments, search]);
+
+
   if (loading === LOADING_STATES.initial || loading === LOADING_STATES.loading) {
     return <span>Cargando</span>;
   }
   return (
     <ul>
       {
-        payments.map((payment) => (
+        paymentsList.map((payment) => (
           <li key={`${payment.date}${payment.cardLastFour}`}>
-            {payment.amount}
+            {`${payment.amount} - ${payment.date} - ${payment.cardLastFour}`}
           </li>
         ))
       }
