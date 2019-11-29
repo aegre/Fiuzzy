317996aa1e57ca2515d0ab79a5965c2e1f36b4c1
Michell Alegría <alegria.trm@gmail.com>
2019-11-29 11:44:58 -0600

    Use moment to convert to date and fix safari bu

--- src/utils/stringToDate.js
+++ src/utils/stringToDate.js
@@ -1,18 +1,15 @@
+import moment from 'moment';
 /**
  * Receives a string and return the converted string in a date
  * @param {string} dateStr
  */
 const stringToDate = (dateStr) => {
-  const [dateSection, hourSection] = dateStr.split('T');
-  const [day, month, year] = dateSection.split('-');
-  const [hour, minutes] = hourSection.split(':');
+  const result = moment(
+    `${dateStr} +00:00`,
+    'DD-MM-YYYY[T]H:m Z',
+  );
 
-  const hourInNumber = Number(hour);
-  const minutesInNumber = Number(minutes);
-
-  const result = new Date(`${year}-${month}-${day}T${hourInNumber < 10 ? `0${hourInNumber}` : hourInNumber}:${minutesInNumber < 10 ? `0${minutesInNumber}` : minutesInNumber}:00+0000`);
-
-  return result;
+  return result.toDate();
 };
 
 export default stringToDate;
